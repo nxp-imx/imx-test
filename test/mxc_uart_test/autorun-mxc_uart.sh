5b5558e4c618ec259842b22906b31acc39fa8875
Andres Lopez <andres.lemos@nxp.com>
2017-03-31 09:01:21 -0500

    MLK-14876: autorun-mxc_uart: Fix hardcoded UART ports
    Make tests dynamic so it would test all available UART ports.
    Eliminate outdated and hardcoded array of numbers.
    
    Signed-off-by: Andres Lopez <andres.lemos@nxp.com>

--- test/mxc_uart_test/autorun-mxc_uart.sh
+++ test/mxc_uart_test/autorun-mxc_uart.sh
@@ -7,14 +7,14 @@ source /unit_tests/test-utils.sh
 #
 STATUS=0
 
-uart_list="0 1 2" ;
+uart_list=($(ls /dev/ttymxc*))
 
-for i in $uart_list; do
-	check_devnode "/dev/ttymxc${i}"
+for i in "${uart_list[@]}"; do
+	check_devnode "${i}"
 done
 
-for i in $uart_list; do
-	run_testcase "./mxc_uart_test.out /dev/ttymxc${i}"
+for i in "${uart_list[@]}"; do
+	run_testcase "./mxc_uart_test.out ${i}"
 done
 
 print_status
