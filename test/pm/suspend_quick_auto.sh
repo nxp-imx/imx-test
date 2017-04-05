bf5e7bd55ce9524c371ae73ff4486ae31f35a8a5
Vanessa Maegima <vanessa.maegima@nxp.com>
2017-04-05 14:37:18 -0300

    MLK-14876: suspend_quick_auto.sh: Fix path to binary
    Fix path to "rtcwakeup.out"
    
    Signed-off-by: Vanessa Maegima <vanessa.maegima@nxp.com>

--- test/pm/suspend_quick_auto.sh
+++ test/pm/suspend_quick_auto.sh
@@ -9,7 +9,7 @@ print_name
 i=0;
 while [ "$i" -lt 20000 ];
 do
-   /unit_tests/rtcwakeup.out -d rtc0 -m mem -s 2;
+   /unit_tests/SRTC/rtcwakeup.out -d rtc0 -m mem -s 2;
    i=`expr $i + 1`;
    echo "==============================="
    echo  suspend $i times
