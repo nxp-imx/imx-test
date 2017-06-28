d017ca7665c798f9b6409d967a83d40ac664a836
Octavian Purdila <octavian.purdila@nxp.com>
2017-06-29 01:35:53 +0300

    MLK-15334-1 test: fix argv definition
    Signed-off-by: Octavian Purdila <octavian.purdila@nxp.com>

--- include/test_utils.h
+++ include/test_utils.h
@@ -1,9 +1,9 @@
-inline void print_name(char *argv[])
+inline void print_name(char * const argv[])
 {
 	printf("\n---- Running < %s > test ----\n\n", argv[0]);
 }
 
-inline void print_result(char *argv[])
+inline void print_result(char * const argv[])
 {
 	printf("\n----  Test < %s > ended  ----\n\n", argv[0]);
 }
--- test/wdog/wdt_driver_test.c
+++ test/wdog/wdt_driver_test.c
@@ -21,7 +21,7 @@
 
 void help_info(void);
 
-int main(int argc, const char *argv[])
+int main(int argc, char * const argv[])
 {
 	int fd, timeout, sleep_sec, test;
 
