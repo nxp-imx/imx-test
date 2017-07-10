88fa2b8509e5e7dbc2a869228be01d635ee4b61d
Octavian Purdila <octavian.purdila@nxp.com>
2017-07-10 14:22:49 +0300

    MLK-15941 bat-rtc: update for imx8
    imx8 does not use a dedicated rtc interrupt, it shares the MU
    interrupt.
    
    Signed-off-by: Octavian Purdila <octavian.purdila@nxp.com>

--- test/bat/bat-rtc.sh
+++ test/bat/bat-rtc.sh
@@ -4,13 +4,33 @@ set -e
 
 batdir=$(dirname $(readlink -f "${BASH_SOURCE[0]}"))
 
+function find_irq()
+{
+    for i in $1; do
+        if ! [ -z "$(grep $i /proc/interrupts)" ]; then
+           echo $i
+           return 0
+        fi
+    done
+    echo "Could not find RTC irq" >&2
+    return 1
+}
+
+irq=$(find_irq "rtc imx8_mu_isr")
+
+if [ "$irq" = "imx8_mu_isr" ]; then
+    check="-ge"
+else
+    check="-eq"
+fi
+
 tmp_file=$(mktemp)
 cpus=$(cat /proc/cpuinfo | grep processor|wc -l)
 
 function sum_irqs()
 {
     s=0
-    for i in $(grep rtc /proc/interrupts|tr -s ' ' |cut -f2- -d:|cut -f2-$((1+cpus)) -d' '); do
+    for i in $(grep $irq /proc/interrupts|tr -s ' ' |cut -f2- -d:|cut -f2-$((1+cpus)) -d' '); do
         s=$((s+i))
     done
     echo $s
@@ -24,7 +44,7 @@ expected_irqs=$(tail -n2 $tmp_file | egrep -o '[0-9]*')
 
 rm $tmp_file
 
-if ! [ $[$end_irqs - $start_irqs] -eq $expected_irqs ]; then
+if ! [ $[$end_irqs - $start_irqs] $check $expected_irqs ]; then
     echo "irq missmatch: got $[$end_irqs - $start_irqs] , expected $expected_irqs"
     exit 1
 fi
