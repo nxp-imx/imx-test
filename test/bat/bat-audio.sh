feb5f8589a36a0266f79673d2de91e6ebeb43def
Mihai Serban <mihai.serban@nxp.com>
2017-08-07 13:26:08 +0300

    MLK-14764-1: Add more boards to audio BAT
    Run the audio BAT on all existing boards.
    
    Signed-off-by: Ion-Horia Petrisor <ion-horia.petrisor@nxp.com>
    Signed-off-by: Mihai Serban <mihai.serban@nxp.com>
    Reviewed-by: Daniel Baluta <daniel.baluta@nxp.com>

--- test/bat/bat-audio.sh
+++ test/bat/bat-audio.sh
@@ -10,11 +10,26 @@ duration=1
 
 # check available audio cards
 case $machine in
+'Freescale i.MX6 DualLite/Solo SABRE Automotive Board'|\
+'Freescale i.MX6 Quad Plus SABRE Automotive Board'\
+)
+    expected_cards_no=3
+    ;;
+'Freescale i.MX6 Quad SABRE Automotive Board'|\
 'Freescale i.MX6 SoloX SDB RevB Board'|\
+'Freescale i.MX6 SoloLite EVK Board'|\
+'Freescale i.MX6 DualLite SABRE Smart Device Board'|\
+'Freescale i.MX6 Quad SABRE Automotive Board'|\
+'Freescale i.MX6 Quad SABRE Smart Device Board'|\
+'Freescale i.MX6 Quad Plus SABRE Smart Device Board'|\
 'Freescale i.MX7 SabreSD Board'\
 )
     expected_cards_no=2
     ;;
+'Freescale i.MX8QXP LPDDR4 ARM2'|\
+'Freescale i.MX8QM ARM2'|\
+'Freescale i.MX8MQ EVK'|\
+'NXP i.MX7ULP EVK'|\
 'Freescale i.MX6 UltraLite 14x14 EVK Board'|\
 'Freescale i.MX6 ULL 14x14 EVK Board'|\
 'Freescale i.MX6SLL EVK Board'\
@@ -31,6 +46,10 @@ esac
 echo "Available audio cards: $cards_no expected: $expected_cards_no"
 
 if [ $cards_no -ne $expected_cards_no ]; then
+    echo "machine: ${machine}"
+    echo "cards:"
+    echo "$(ls -ld /proc/asound/card* | grep ^d)"
+    echo "$(cat /proc/asound/cards)"
     exit 1
 fi
 
