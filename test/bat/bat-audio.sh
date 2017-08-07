326bb0bc57e729ee2731e6cd9300674cda13da59
Mihai Serban <mihai.serban@nxp.com>
2017-08-07 13:28:38 +0300

    MLK-14764-2: bat: Add playback/capture tests to audio BAT
    For every audio cards found in a board run a simple playback or capture
    test to check its basic functionality. Because these tests are executed
    by CI for every new patch we want them finish as fast as possible so
    we limit a test for a device to run only two scenarios:
    1. one second, first supported rate, format and channels
    2. one second, last supported rate, format and channels
    
    For now HDMI audio cards are skipped because we do not have HDMI
    devices attached to the boards under test.
    
    Signed-off-by: Ion-Horia Petrisor <ion-horia.petrisor@nxp.com>
    Signed-off-by: Mihai Serban <mihai.serban@nxp.com>
    Reviewed-by: Daniel Baluta <daniel.baluta@nxp.com>

--- test/bat/bat-audio.sh
+++ test/bat/bat-audio.sh
@@ -2,10 +2,10 @@
 
 batdir=$(dirname $(readlink -f "${BASH_SOURCE[0]}"))
 . $batdir/bat_utils.sh
+alsatool=$batdir/../Audio/mxc_alsa_hw_params.out
 
 cards_no=$(ls -ld /proc/asound/card* | grep ^d | wc -l)
 machine=$(cat /sys/devices/soc0/machine)
-sample_wav=/unit_tests/ASRC/audio8k16S.wav
 duration=1
 
 # check available audio cards
@@ -53,15 +53,61 @@ if [ $cards_no -ne $expected_cards_no ]; then
     exit 1
 fi
 
-# check aplay on each device of each existing card
+echo ""
+# Check aplay/arecord on each device of each existing card. Run two
+# scenarios: with the first supported rate, format, channel and
+# last supported rate, format, channel.
+# perform all tests and return 0 upon success or last faulty exit code
+ecode=0
 while read line; do
     dev=$(echo $line | cut -d ':' -f1 | tr - ,)
+    echo "Testing hw:$line"
 
-    aplay -Dhw:$dev -d $duration $sample_wav
-
-    if [ $? -ne 0 ]; then
-        exit $?
+    # skip HDMI and S/PDIF devices since we need additional connectors
+    # for them to be attached to DUTs
+    if grep -qi hdmi <<< $line; then
+        echo "hw:$dev device is HDMI. aplay/arecord test will be skipped"
+        continue
     fi
+    if grep -qi "S/PDIF" <<< $line; then
+        echo "hw:$dev device is S/PDIF. aplay/arecord test will be skipped"
+        continue
+    fi
+    if grep -qi "rpmsg" <<< $line; then
+        echo "hw:$dev device is RPMSG. Skip aplay/arecord tests for now"
+        continue
+    fi
+
+    # check if the device can perform playback and test it
+    if [[ $line =~ "playback" ]]; then
+        declare -a rate=($($alsatool hw:$dev p r))
+        declare -a format=($($alsatool hw:$dev p f))
+        declare -a channel=($($alsatool hw:$dev p c))
+        aplay -Dhw:$dev -r ${rate[0]} -f ${format[0]} -c ${channel[0]} -d $duration -t raw /dev/zero
+        result=$?
+        [ $result -ne 0 ] && ecode=$result
+        aplay -Dhw:$dev -r ${rate[${#rate[@]}-1]} -f ${format[${#format[@]}-1]} -c ${channel[${#channel[@]}-1]} -d $duration -t raw /dev/zero
+        result=$?
+        [ $result -ne 0 ] && ecode=$result
+    fi
+
+    # check if the device can perform record and test it
+    if [[ $line =~ "capture" ]]; then
+        if grep -qi "HiFi-AMIX" <<< $line; then
+            echo "hw:$dev device is AMIX. Skip simple capture test"
+            continue
+        fi
+        declare -a rate=($($alsatool hw:$dev c r))
+        declare -a format=($($alsatool hw:$dev c f))
+        declare -a channel=($($alsatool hw:$dev c c))
+        arecord -Dhw:$dev -r ${rate[0]} -f ${format[0]} -c ${channel[0]} -d $duration -t raw /dev/null
+        result=$?
+        [ $result -ne 0 ] && ecode=$result
+        arecord -Dhw:$dev -r ${rate[${#rate[@]}-1]} -f ${format[${#format[@]}-1]} -c ${channel[${#channel[@]}-1]} -d $duration -t raw /dev/null
+        result=$?
+        [ $result -ne 0 ] && ecode=$result
+    fi
+    echo ""
 done <  /proc/asound/pcm
 
-exit 0
+exit $ecode
