979a3df3f7dd79e378e956ba2a0cfd6ada0fe0a4
Daniel Baluta <daniel.baluta@nxp.com>
2017-10-12 16:26:46 +0300

    MLK-13946: bat-audio: Increase expected audio cards on imx8qm
    With commit 9ca93ac9ab677874 ("MLK-13946-5: ARM64: dts: enable
    dp audio for imx8qm") we now have an audio HDMI interface, so
    we need to increase the expected audio cards.
    
    Signed-off-by: Daniel Baluta <daniel.baluta@nxp.com>
    Reviewed-by: Mihai Serban <mihai.serban@nxp.com>

--- test/bat/bat-audio.sh
+++ test/bat/bat-audio.sh
@@ -16,6 +16,7 @@ case $machine in
     expected_cards_no=3
     ;;
 'Freescale i.MX8MQ EVK'|\
+'Freescale i.MX8QM ARM2'|\
 'Freescale i.MX6 Quad SABRE Automotive Board'|\
 'Freescale i.MX6 SoloX SDB RevB Board'|\
 'Freescale i.MX6 SoloLite EVK Board'|\
@@ -28,7 +29,6 @@ case $machine in
     expected_cards_no=2
     ;;
 'Freescale i.MX8QXP LPDDR4 ARM2'|\
-'Freescale i.MX8QM ARM2'|\
 'NXP i.MX7ULP EVK'|\
 'Freescale i.MX6 UltraLite 14x14 EVK Board'|\
 'Freescale i.MX6 ULL 14x14 EVK Board'|\
