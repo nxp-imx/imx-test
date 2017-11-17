4a63dd2bceead29d956e1efc3d8cd6327ded3d4b
Daniel Baluta <daniel.baluta@nxp.com>
2017-11-17 11:17:52 +0200

    MLK-16892: bat-audio: Move board specific probe to bat-audio-probe.sh
    Even if probe fails for some cards we still want to test the rest of them.
    
    Signed-off-by: Daniel Baluta <daniel.baluta@nxp.com>
    Reviewed-by: Cosmin Samoila <cosmin.samoila@nxp.com>

---
+++ test/bat/bat-audio-probe.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+#
+# Check that the expected sound cards are successfully registered
+#
+
+batdir=$(dirname $(readlink -f "${BASH_SOURCE[0]}"))
+. $batdir/bat_utils.sh
+
+cards_no=$(ls -ld /proc/asound/card* | grep ^d | wc -l)
+machine=$(cat /sys/devices/soc0/machine)
+
+# check available audio cards
+case $machine in
+'Freescale i.MX8MQ EVK'|\
+'Freescale i.MX6 DualLite/Solo SABRE Automotive Board'|\
+'Freescale i.MX6 Quad Plus SABRE Automotive Board'\
+)
+    expected_cards_no=3
+    ;;
+'Freescale i.MX8QM ARM2'|\
+'Freescale i.MX6 Quad SABRE Automotive Board'|\
+'Freescale i.MX6 SoloX SDB RevB Board'|\
+'Freescale i.MX6 SoloLite EVK Board'|\
+'Freescale i.MX6 DualLite SABRE Smart Device Board'|\
+'Freescale i.MX6 Quad SABRE Automotive Board'|\
+'Freescale i.MX6 Quad SABRE Smart Device Board'|\
+'Freescale i.MX6 Quad Plus SABRE Smart Device Board'|\
+'Freescale i.MX7 SabreSD Board'\
+)
+    expected_cards_no=2
+    ;;
+'Freescale i.MX8QXP LPDDR4 ARM2'|\
+'NXP i.MX7ULP EVK'|\
+'Freescale i.MX6 UltraLite 14x14 EVK Board'|\
+'Freescale i.MX6 ULL 14x14 EVK Board'|\
+'Freescale i.MX6SLL EVK Board'\
+)
+    expected_cards_no=1
+    ;;
+*)
+    expected_cards_no=0
+    echo "Unknown machine '$machine'"
+    exit $BAT_EXITCODE_SKIP
+    ;;
+esac
+
+echo "Available audio cards: $cards_no expected: $expected_cards_no"
+
+if [ $cards_no -ne $expected_cards_no ]; then
+    echo "machine: ${machine}"
+    echo "cards:"
+    echo "$(ls -ld /proc/asound/card* | grep ^d)"
+    echo "$(cat /proc/asound/cards)"
+    exit 1
+fi
+
+exit 0
--- test/bat/bat-audio.sh
+++ test/bat/bat-audio.sh
@@ -8,52 +8,6 @@ cards_no=$(ls -ld /proc/asound/card* | grep ^d | wc -l)
 machine=$(cat /sys/devices/soc0/machine)
 duration=1
 
-# check available audio cards
-case $machine in
-'Freescale i.MX8MQ EVK'|\
-'Freescale i.MX6 DualLite/Solo SABRE Automotive Board'|\
-'Freescale i.MX6 Quad Plus SABRE Automotive Board'\
-)
-    expected_cards_no=3
-    ;;
-'Freescale i.MX8QM ARM2'|\
-'Freescale i.MX6 Quad SABRE Automotive Board'|\
-'Freescale i.MX6 SoloX SDB RevB Board'|\
-'Freescale i.MX6 SoloLite EVK Board'|\
-'Freescale i.MX6 DualLite SABRE Smart Device Board'|\
-'Freescale i.MX6 Quad SABRE Automotive Board'|\
-'Freescale i.MX6 Quad SABRE Smart Device Board'|\
-'Freescale i.MX6 Quad Plus SABRE Smart Device Board'|\
-'Freescale i.MX7 SabreSD Board'\
-)
-    expected_cards_no=2
-    ;;
-'Freescale i.MX8QXP LPDDR4 ARM2'|\
-'NXP i.MX7ULP EVK'|\
-'Freescale i.MX6 UltraLite 14x14 EVK Board'|\
-'Freescale i.MX6 ULL 14x14 EVK Board'|\
-'Freescale i.MX6SLL EVK Board'\
-)
-    expected_cards_no=1
-    ;;
-*)
-    expected_cards_no=0
-    echo "Unknown machine '$machine'"
-    exit $BAT_EXITCODE_SKIP
-    ;;
-esac
-
-echo "Available audio cards: $cards_no expected: $expected_cards_no"
-
-if [ $cards_no -ne $expected_cards_no ]; then
-    echo "machine: ${machine}"
-    echo "cards:"
-    echo "$(ls -ld /proc/asound/card* | grep ^d)"
-    echo "$(cat /proc/asound/cards)"
-    exit 1
-fi
-
-echo ""
 # Check aplay/arecord on each device of each existing card. Run two
 # scenarios: with the first supported rate, format, channel and
 # last supported rate, format, channel.
