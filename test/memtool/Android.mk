8d2b83df7dbe45881fcbfa9b64886fb33b45e779
guoyin.chen <guoyin.chen@nxp.com>
2018-07-26 16:19:20 +0800

    MA-12239 Add memtool build support for 8mq Android
    Add multilib build to build both 32bit and 64bit memtool
    Add 8mq mx8mq_modules
    
    Change-Id: I254844ef2cd42c4582459e82922082dda69b0816
    Signed-off-by: guoyin.chen <guoyin.chen@nxp.com>

--- test/memtool/Android.mk
+++ test/memtool/Android.mk
@@ -1,4 +1,4 @@
-ifeq ($(BOARD_SOC_CLASS),IMX6)
+ifeq ($(findstring imx, $(TARGET_BOARD_PLATFORM)), imx)
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
@@ -11,7 +11,8 @@ LOCAL_SRC_FILES := \
        mx6ul_modules.c \
        mx7d_modules.c \
        mx6ull_modules.c \
-       mx7ulp_modules.c
+       mx7ulp_modules.c \
+       mx8mq_modules.c
 
 #LOCAL_CFLAGS += -DBUILD_FOR_ANDROID
 
@@ -21,6 +22,10 @@ LOCAL_C_INCLUDES += $(LOCAL_PATH) \
 LOCAL_SHARED_LIBRARIES := libutils libc
 
 LOCAL_MODULE := memtool
+LOCAL_MULTILIB := both
 LOCAL_MODULE_TAGS := tests
+LOCAL_MODULE_STEM_32 := memtool_32
+LOCAL_MODULE_STEM_64 := memtool_64
+LOCAL_VENDOR_MODULE := true
 include $(BUILD_EXECUTABLE)
 endif
