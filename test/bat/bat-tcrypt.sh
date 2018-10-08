0d2d5c703e9f6ffb3a48711745521fecfc55703d
Leonard Crestez <leonard.crestez@nxp.com>
2018-10-08 16:39:17 +0300

    bat-tcrypt.sh: New test doing modprobe tcrypt
    This test tolerates missing algs or tests.
    
    Signed-off-by: Leonard Crestez <leonard.crestez@nxp.com>
    Reviewed-by: Horia Geantă <horia.geanta@nxp.com>

---
+++ test/bat/bat-tcrypt.sh
@@ -0,0 +1,38 @@
+#! /bin/bash
+
+batdir=$(dirname $(readlink -f "${BASH_SOURCE[0]}"))
+. $batdir/bat_utils.sh
+
+if ! modinfo tcrypt > /dev/null; then
+    echo "Missing tcrypt module?"
+    exit $BAT_EXITCODE_SKIP
+fi
+
+dmesg -c > /dev/null
+modprobe tcrypt || TCRYPT_RESULT=$?
+
+fail_tolerated=0
+fail_unexpected=0
+while read -r line; do
+    if [[ $line == *" Failed to load transform "* ]]; then
+        fail_tolerated=1
+    elif [[ $line == *" Failed to load tfm "* ]]; then
+        fail_tolerated=1
+    elif [[ $line == *"alg: No test for "* ]]; then
+        fail_tolerated=1
+    else
+        fail_unexpected=1
+        echo -E "$line"
+    fi
+done < <(dmesg | grep alg: )
+
+if [[ $fail_unexpected != 0 ]]; then
+    echo "tcrypt reported unexpected failures"
+    exit $BAT_EXITCODE_FAIL
+fi
+if [[ $TCRYPT_RESULT == 0 && $fail_tolerated == 0 ]]; then
+    echo "tcrypt passed perfectly"
+else
+    echo "tcrypt passed other than missing algs/tests"
+fi
+echo $BAT_EXITCODE_PASS
