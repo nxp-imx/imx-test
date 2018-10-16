4cba366a8ae71404ef03b2a60d3b80198990c4ee
Daniel Baluta <daniel.baluta@nxp.com>
2018-10-16 15:17:56 +0300

    MLK-19954: bat: Add bat-audio-dsp.sh
    This adds a simple sanity test for DSP on imx8QXP
    boards (MEK/arm2).
    
    The test decodes a small mp3 file and compares the
    output with a reference file.
    
    Reviewed-by: Cosmin-Gabriel Samoila <cosmin.samoila@nxp.com>
    Signed-off-by: Daniel Baluta <daniel.baluta@nxp.com>

---
+++ test/bat/bat-audio-dsp.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+batdir=$(dirname $(readlink -f "${BASH_SOURCE[0]}"))
+. $batdir/bat_utils.sh
+
+machine=$(cat /sys/devices/soc0/machine)
+
+# filter for supported platforms
+case $machine in
+'Freescale i.MX8QXP MEK'|\
+'Freescale i.MX8QXP LPDDR4 ARM2'\
+)
+    ;;
+*)
+    echo "DSP not supported on '$machine'"
+    exit $BAT_EXITCODE_SKIP
+    ;;
+esac
+
+function check_binaries()
+{
+    if [ ! -f /lib/firmware/imx/dsp/hifi4.bin ]; then
+        exit $BAT_EXITCODE_FAIL
+    fi
+
+    if [ ! -f /usr/lib/imx-mm/audio-codec/dsp/lib_dsp_codec_wrap.so ]; then
+        exit $BAT_EXITCODE_FAIL
+    fi
+
+    if [ ! -f /usr/lib/imx-mm/audio-codec/dsp/lib_dsp_mp3_dec.so ]; then
+        exit $BAT_EXITCODE_FAIL
+    fi
+}
+
+#param: $1 - file type, $2 - input file, $3 - reference file
+function do_decode()
+{
+    XXX_OUT_FILE=$2.out
+    dsp_test -f$1 -i$2 -o$XXX_OUT_FILE
+
+    diff $3 $XXX_OUT_FILE &> /dev/null
+    res=$?
+    rm -f $XXX_OUT_FILE
+
+    if [[ $res == 0 ]]; then
+        exit 0
+    else
+         exit $BAT_EXITCODE_FAIL
+    fi
+}
+
+# MP3 decode
+MP3_IN_FILE=/unit_tests/Audio/hihat_9_frames.mp3
+MP3_REF_FILE=/unit_tests/Audio/hihat_9_frames.mp3.pcm
+
+check_binaries
+
+do_decode 1 $MP3_IN_FILE $MP3_REF_FILE
