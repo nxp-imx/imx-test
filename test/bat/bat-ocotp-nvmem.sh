315bdb0987bab1e0e7d969ccd006e100164f8c2a
Leonard Crestez <leonard.crestez@nxp.com>
2019-04-15 23:51:23 +0300

    bat-ocotp-nvmem.sh: New script testing nvmem
    The FSL_OTP driver for /sys/fsl_otp is very old and should be dropped.
    
    Test the ocotp fuse driver for the new nvmem subsystem.
    
    Signed-off-by: Leonard Crestez <leonard.crestez@nxp.com>
    Reviewed-by: Daniel Baluta <daniel.baluta@nxp.com>

---
+++ test/bat/bat-ocotp-nvmem.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+set -e
+. $(dirname $(readlink -f "${BASH_SOURCE[0]}"))/bat_utils.sh
+
+IMX_NVMEM_FILE=/sys/bus/nvmem/devices/imx-ocotp0/nvmem
+
+# Check existence:
+if [[ ! -f $IMX_NVMEM_FILE ]]; then
+    echo "Missing $IMX_NVMEM_FILE?"
+    exit $BAT_EXITCODE_FAIL
+fi
+
+echo "Dump first few fuses from imx-ocotp:"
+od -v -tx4 --endian=little "$IMX_NVMEM_FILE" --read-bytes=128
+
+# Check read works until the end
+if ! dd if="$IMX_NVMEM_FILE" of=/dev/null bs=16; then
+    echo "Failed reading $IMX_NVMEM_FILE"
+    exit $BAT_EXITCODE_FAIL
+fi
+
+exit $BAT_EXITCODE_PASS
