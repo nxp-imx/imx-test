101707c0059cc70d6fb03f32d974495de0f14fac
Ming Qian <ming.qian@nxp.com>
2021-01-05 14:03:00 +0800

    MLK-25213:[8QM_MEK/8QXP_MEK]mxc:vpu: unittest update copyright
    Signed-off-by: Ming Qian <ming.qian@nxp.com>

--- test/mxc_v4l2_vpu_test/pitcher/queue.h
+++ test/mxc_v4l2_vpu_test/pitcher/queue.h
@@ -1,7 +1,19 @@
 /*
- * Copyright 2018 NXP
+ * Copyright 2018-2021 NXP
  *
- * include/queue.h
+ */
+
+/*
+ * The code contained herein is licensed under the GNU General Public
+ * License. You may obtain a copy of the GNU General Public License
+ * Version 2 or later at the following locations:
+ *
+ * http://www.opensource.org/licenses/gpl-license.html
+ * http://www.gnu.org/copyleft/gpl.html
+ */
+
+/*
+ * queue.h
  *
  * Author Ming Qian<ming.qian@nxp.com>
  */
